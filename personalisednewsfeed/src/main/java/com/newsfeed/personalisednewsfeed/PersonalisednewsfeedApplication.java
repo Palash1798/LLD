0739011765111d83799c4d722874e9c6fe608eaa
Palash4567 <palash.khobragade@humancloud.co.in>
2026-09-11 20:48:38 +0530

    init for personalised news feed

---
+++ personalisednewsfeed/src/main/java/com/newsfeed/personalisednewsfeed/PersonalisednewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.newsfeed.personalisednewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalisednewsfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalisednewsfeedApplication.class, args);
+	}
+
+}
---
+++ personalisednewsfeed/src/test/java/com/newsfeed/personalisednewsfeed/PersonalisednewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsfeed.personalisednewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalisednewsfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
